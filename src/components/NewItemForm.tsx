95fb4db486260f53ac6186456d3cfe564605d27c
arsimmsejdiu <arsimmsejdiu@gmail.com>
2022-01-31 19:08:03 +0100

    adding the context api to the app

--- src/components/NewItemForm.tsx
+++ src/components/NewItemForm.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { NewItemButton, NewItemFormContainer, NewItemInput } from "../styles";
-import { useFocus } from '../utils/useFocus';
+import { useFocus } from "../utils/useFocus";
 
 // onAdd is a callback passed through AddNewItemProps.
 type NewItemFormProps = {
@@ -11,13 +11,25 @@ export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
   const [text, setText] = useState("");
   const inputRef = useFocus();
 
+  const handleAddText = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      onAdd(text);
+      //setText("");
+    }
+  };
+
   return (
     // The component uses a controlled input. We’ll store the value for it in the text state.
     // Whenever you type in the text inside this input, the text state is updated.
     // Here we didn’t have to provide any type for the event argument of our onChange
     // callback. TypeScript gets the type from React type definitions
     <NewItemFormContainer>
-      <NewItemInput ref={inputRef} value={text} onChange={(e) => setText(e.target.value)} />
+      <NewItemInput
+        ref={inputRef}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        onKeyPress={handleAddText}
+      />
       <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
     </NewItemFormContainer>
   );
